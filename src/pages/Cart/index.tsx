59283a90b16e77eae6b1312af5cc431da66d4f26
Your Name <bsromeiro@gmail.com>
2020-09-10 22:06:34 -0300

    Update: Move components to Cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,19 +1,64 @@
 import React from 'react';
-import Cards from '../../components/Cards'
-import Total from '../../components/Total'
-import Buttom from '../../components/Buttom'
 
-import {Container} from './styles'
+import {CartWrapper, CardsWrapper, ProductTitle, ItemTitle, CardList, Price, Product, TotalCard, PriceList, PriceItem, TotalPrice, Discount, ButtomIcon } from './styles'
 
 const Cart: React.FC = () => (
   <>
     <h1>Cart</h1>
 
-    <Container>
-      <Cards />
-      <Total />
-    <Buttom />
-    </Container>
+    <CartWrapper>
+
+      {/* <Cards /> */}
+      <CardsWrapper>
+        <ProductTitle>Produtos</ProductTitle>
+        <CardList>
+          <Product>
+            <img
+              src="https://i.imgur.com/0Hd73Qe.png"
+              alt="L'oreal Professionnel"
+            />
+            <div>
+              <ItemTitle>L'oreal Professionnel Expert Absolut Repair Cortex Lipidium</ItemTitle>
+              <Price>R$ 299,00</Price>
+            </div>
+          </Product>
+        </CardList>
+      </CardsWrapper>
+
+
+      {/* <Total /> */}
+      <TotalCard>
+        <PriceList>
+
+          <PriceItem>
+            <p>Produtos</p>
+            <p>R$ 624,50</p>
+          </PriceItem>
+
+          <PriceItem>
+            <p>Frete</p>
+            <p>R$ 5,30</p>
+          </PriceItem>
+
+          <Discount>
+            <p>Desconto</p>
+            <p>- R$ 30,0</p>
+          </Discount>
+
+          <TotalPrice>
+            <p>Total</p>
+            <p>R$ 600,10</p>
+          </TotalPrice>
+        </PriceList>
+
+      </TotalCard>
+
+
+      {/* <Buttom /> */}
+      <ButtomIcon>
+        Seguir para o pagamento
+      </ButtomIcon>
+    </CartWrapper>
   </>
 );
 
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,5 +1,143 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const CartWrapper = styled.div`
   padding: 10px;
 `;
+
+
+// Product Card
+export const CardsWrapper = styled.div``;
+export const ProductTitle = styled.text`
+  height: 17px;
+  text-transform: uppercase;
+  display: flex;
+  margin: 5px 20px;
+  flex-grow: 1;
+  color: #999;
+  font-family: "Helvetica Neue";
+  font-size: 14px;
+  font-weight: 700;
+  letter-spacing: NaNpx;
+  line-height: 17px;
+`;
+export const CardList = styled.div`
+  margin: 0 auto;
+  padding: 0.1px 12px;
+  border-radius: 3px;
+  background-color: #FFF;
+  box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+`;
+
+export const Product = styled.div`
+  margin: 15px auto;
+  height: 90px;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  border-radius: 3px;
+  border: 1px solid #eee;
+  img {
+    height: 65px;
+    width: 65px;
+    margin: 11px 11px 14px;
+    justify-content: center;
+  }
+  div {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 12px;
+    padding: 12px 0;
+  }
+`;
+
+export const ItemTitle = styled.text`
+  height: 32px;
+  color: #212122;
+  font-family: "Helvetica Neue";
+  font-size: 13px;
+  letter-spacing: NaNpx;
+  line-height: 16px;
+`;
+
+export const Price = styled.text`
+  height: 17px;
+  color: #212122;
+  font-family: "Helvetica Neue";
+  font-size: 14px;
+  font-weight: 700;
+  letter-spacing: NaNpx;
+  line-height: 17px;
+  text-align: right;
+  margin-bottom: 2px;
+`;
+
+// Total Card
+export const TotalCard = styled.div`
+  padding: 6px 13px 13px 14px;
+  margin: 20px auto;
+  height: 129px;
+  box-sizing: border-box;
+  height: 129px;
+  border-radius: 3px;
+  border: 1px solid #CCC;
+`;
+export const PriceList = styled.div`
+  height: 100%;
+`;
+export const PriceItem = styled.div`
+  margin: 9px 0;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: space-between;
+  color: #212122;
+  font-family: "Helvetica Neue";
+  font-size: 14px;
+  letter-spacing: NaNpx;
+`;
+export const Discount = styled.div`
+  margin: 9px 0;
+  text-transform: uppercase;
+  color: #FF7800;
+  display: flex;
+  justify-content: space-between;
+  font-family: "Helvetica Neue";
+  font-size: 14px;
+  letter-spacing: NaNpx;
+`;
+
+export const TotalPrice = styled.div`
+  text-transform: uppercase;
+  margin-top: 16px;
+  display: flex;
+  justify-content: space-between;
+  color: #212122;
+  font-family: "Helvetica Neue";
+  font-size: 14px;
+  letter-spacing: NaNpx;
+  font-weight: 700;
+`;
+
+// Button
+export const ButtomIcon = styled.button`
+  height: 60px;
+  width: 100%;
+  border-radius: 3px;
+  background-color: #FF6C00;
+  box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25);
+  border: 0;
+  color: #FFF;
+  font-family: "Helvetica Neue";
+  font-size: 20px;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  line-height: 24px;
+  text-align: center;
+  text-transform: uppercase;
+  :hover {
+    background: #D45A00;
+  }
+`;
+
