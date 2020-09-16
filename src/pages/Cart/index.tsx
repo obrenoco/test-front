8558c6933e036c59b449934aa437c4ebb2131846
Your Name <bsromeiro@gmail.com>
2020-09-16 12:18:58 -0300

    Add: Localstorage on Products and Prices

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -27,8 +27,43 @@ interface Price {
 }
 
 const Cart: React.FC = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [price, setPrice] = useState<Price | null>(null);
+  const [items, setItems] = useState<Item[]>(() => {
+    const storagedItems = localStorage.getItem(
+      '@BelezaNaWeb:products',
+    )
+
+    if (storagedItems) {
+      return JSON.parse(storagedItems);
+    } else {
+      return []
+    }
+  });
+
+  const [price, setPrice] = useState<Price | null>(() => {
+    const storagedPrices = localStorage.getItem(
+      '@BelezaNaWeb:prices',
+    )
+
+    if (storagedPrices) {
+      return JSON.parse(storagedPrices);
+    } else {
+      return []
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@BelezaNaWeb:products',
+      JSON.stringify(items),
+    )
+  },[items]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@BelezaNaWeb:prices',
+        JSON.stringify(price),
+    )
+  }, [price]);
 
   useEffect(() => {
     api.get('5b15c4923100004a006f3c07').then((response) => {
@@ -49,7 +84,6 @@ const Cart: React.FC = () => {
   // Total price hightlight
   return (
     <>
-
       {/* <Header /> */}
       <Header>
         <ul>
@@ -61,22 +95,21 @@ const Cart: React.FC = () => {
 
       <CartWrapper>
         {/* <Cards /> */}
-          <h2>Produtos</h2>
-          <ProductsList>
-            {items.map(item => (
-              <Product key={item.product.sku}>
-                <img
-                  src={item.product.imageObjects[0].small}
-                  alt={item.product.name}
-                />
-                <div>
-                  <p>{item.product.name}</p>
-                  <strong>R$ {item.product.priceSpecification.price}</strong>
-                </div>
-              </Product>
-            ))}
-          </ProductsList>
-
+        <h2>Produtos</h2>
+        <ProductsList>
+          {items.map(item => (
+            <Product key={item.product.sku}>
+              <img
+                src={item.product.imageObjects[0].small}
+                alt={item.product.name}
+              />
+              <div>
+                <p>{item.product.name}</p>
+                <strong>R$ {item.product.priceSpecification.price}</strong>
+              </div>
+            </Product>
+          ))}
+        </ProductsList>
 
         {/* <Total /> */}
         <TotalCard>
